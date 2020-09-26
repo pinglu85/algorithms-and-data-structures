e36d367ed241c5d3f2459585797d56d8222aa1b5
pinglu85 <luping8576@hotmail.com>
2020-09-26 23:39:23 +0200

    initial commit

---
+++ algorithms/sorting_algorithms/bubbleSort.js
@@ -0,0 +1,21 @@
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap
+        // const temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+const array = [8, 1, 2, 3, 4, 5, 6, 7];
+console.log(bubbleSort(array));
