631d21019ac2fad90023cc42fbb1db82a016f7c9
pinglu85 <luping8576@hotmail.com>
2020-10-06 05:38:04 +0200

    refactor the code

--- algorithms/sorting_algorithms/insertionSort.js
+++ algorithms/sorting_algorithms/insertionSort.js
@@ -1,8 +1,13 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
-      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+    const currentVal = arr[i];
+    let j = i - 1;
+    // for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--)
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
