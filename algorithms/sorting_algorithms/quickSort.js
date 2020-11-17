d14db0ba03d571dfeb138b37d40878b70f240782
pinglu85 <luping8576@hotmail.com>
2020-11-17 22:31:22 +0100

    Initial commit

---
+++ algorithms/sorting_algorithms/quickSort.js
@@ -0,0 +1,26 @@
+function pivot(arr) {
+  const pivot = arr[0];
+  let swapIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      swapIndex++;
+      [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
+    }
+  }
+  [arr[0], arr[swapIndex]] = [arr[swapIndex], arr[0]];
+  return swapIndex;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const copied = [...arr];
+  const pivotIndex = pivot(copied);
+  const left = mergeSort(copied.slice(0, pivotIndex));
+  const right = mergeSort(copied.slice(pivotIndex + 1));
+  return left.concat(copied[pivotIndex], right);
+}
+
+const arr = [4, 8, 2, 1, 5, 7, 6, 3];
+console.log(mergeSort(arr));
