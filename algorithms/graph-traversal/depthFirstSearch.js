5c4069e294a171cf9a20eecc4001b21d60d5b387
pinglu85 <luping8576@hotmail.com>
2021-03-22 21:51:41 +0100

    add recursive implementation.

--- algorithms/graph-traversal/depthFirstSearch.js
+++ algorithms/graph-traversal/depthFirstSearch.js
@@ -31,3 +31,27 @@ function depthFirstSearch(graph, source) {
 
   return [...visited];
 }
+
+/**
+ * Performs Depth First Search on a graph from the given source code with recursion.
+ * @param {object} graph A graph represented with adjacency list.
+ * Key of the object holds a vertex and value holds an array of adjacent nodes.
+ * @param {number|string} source The source node to start traversal from.
+ * @returns {*[]} A DFS-traversed order of nodes.
+ */
+function recursiveDepthFirstSearch(graph, source) {
+  const recursiveDFSImpl = (graph, node, visited) => {
+    const neighbors = graph[node];
+    visited.add(node);
+
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        recursiveDFSImpl(graph, neighbor, visited);
+      }
+    }
+
+    return [...visited];
+  };
+
+  return recursiveDFSImpl(graph, source, new Set());
+}
